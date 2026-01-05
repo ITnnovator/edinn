067be44cedf522b34f46c3e996cecd73980626f4
Arish Ali <109577410+arishhh@users.noreply.github.com>
2026-01-06 00:41:39 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
 
+        {/* Local CSS */}
+        <link rel="stylesheet" type="text/css" href="/css/main.min.css" />
+        <link rel="stylesheet" type="text/css" href="/css/button.min.css" />
+
 
 
 
